fb1639319273468087ea66517e95e43a6a1de7d5
dinglei <dinglei@banggood.cn>
2018-08-23 08:11:49 +0800

    add something

---
+++ Chapter2:Multi-armedBandits/k_armed_bandit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-8-23 上午6:38
+# @Author  : BigDin
+# @Contact : dinglei_1107@outlook.com
+
+import numpy as np
+import random
+
+
+class SimpleBandit:
+
+    """
+    价值函数q_*(a)：给定行为a下获得的平均奖励——
+    the true value of an action is the mean reward when that action is selected
+
+    行为：在第t期的选择记为A_t
+
+    实际中无法知道真实的价值函数，用估值函数Q(A_t)进行行为选择
+    真实奖励：在第t期对行为A_t的奖励服从均值为q_*(A_t)，方差为1的正态分布
+    """
+
+    def __init__(self, k=10, epsilon=0):
+        self.__k = k
+        self.__epsilon = epsilon
+
+        self.__action_counter = dict(zip(range(1, k+1), [0]*k))
+        self.__value_estimate = dict(zip(range(1, k+1), [0]*k))
+
+    def random_choice(self):
+        return random.randint(1, self.__k)
+
+    def reward(self, action):
+        return random.normalvariate(self.__value_estimate[action], 1)
